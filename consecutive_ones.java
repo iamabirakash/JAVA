b354e0ced3f28a2d2d9e1f33111d00e0d9b65f31
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-06-30 16:12:43 +0530

    consecutive_ones

---
+++ consecutive_ones.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class consecutive_ones {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                if (currentCount > maxCount) {
+                    maxCount = currentCount;
+                }
+            } else {
+                currentCount = 0;
+            }
+        }
+        
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        int result = findMaxConsecutiveOnes(nums);
+        System.out.println("The maximum number of consecutive 1s is: " + result); // Output: 3
+    }
+}
