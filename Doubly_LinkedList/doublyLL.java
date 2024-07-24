1c22d3430f14c1130c38e3a4cd5ece78ddcb7001
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-24 10:16:23 +0530

    53th CW | display head, tail, random

---
+++ Doubly_LinkedList/doublyLL.java
@@ -0,0 +1,66 @@
+package Doubly_LinkedList;
+
+public class doublyLL {
+    public static class Node {
+        int val;
+        Node next;
+        Node prev;
+        Node(int val){
+            this.val = val;
+        }
+    }
+
+    public static void display(Node head){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    
+    public static void displayrev(Node tail){
+        Node temp = tail;
+        while(temp!=null){
+            System.out.print(temp.val + " ");
+            temp = temp.prev;
+        }
+        System.out.println();
+    }
+
+    public static void displayrandom(Node random){ //we have to print starting to end
+        Node temp = random;
+        // Move this temp backwards to the head
+        while(temp.prev!=null){
+            temp = temp.prev;
+        }
+        // now temp is at head
+        while(temp!=null){
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        //4 10 2 99 13
+        Node a = new Node(4);
+        Node b = new Node(10);
+        Node c = new Node(2);
+        Node d = new Node(99);
+        Node e = new Node(13);
+        a.prev = null;
+        a.next = b;
+        b.prev = a;
+        b.next = c;
+        c.prev = b;
+        c.next = d;
+        d.prev = c;
+        d.next = e;
+        e.prev = d;
+        e.next =  null;
+        // display(a);
+        // displayrev(e);
+        displayrandom(c);
+    }
+}
