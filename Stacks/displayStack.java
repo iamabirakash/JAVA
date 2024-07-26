71bfbe972e1fea358f5ad8a9954311b26daece83
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-26 10:24:14 +0530

    54th CW | Added display recursion

--- Stacks/displayStack.java
+++ Stacks/displayStack.java
@@ -3,10 +3,21 @@ package Stacks;
 import java.util.Stack;
 
 public class displayStack {
-    public static void displayRec(Stack<Integer> s){
-        s.push(6);
+    public static void displayRevRec(Stack<Integer> st){
+        if(st.size()==0) return;
+        int top = st.pop();
+        System.out.print(top+ " ");
+        displayRevRec(st);
+        st.push(top);
     }
-
+    
+    public static void displayRec(Stack<Integer> st){
+        if(st.size()==0) return;
+        int top = st.pop();
+        displayRec(st);
+        System.out.print(top+ " ");
+        st.push(top);
+    }
     public static void main(String[] args) {
         Stack<Integer> st = new Stack<>();
         st.push(1);
@@ -14,8 +25,8 @@ public class displayStack {
         st.push(3);
         st.push(4);
         st.push(5);
-        System.out.println(st);
+        displayRevRec(st);
+        System.out.println();
         displayRec(st);
-        System.out.println(st);
     }
 }
