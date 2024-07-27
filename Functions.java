d7188ed024b6adf62db82394a89d5685f0ddaef6
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-27 10:44:39 +0530

    Corrected Error

--- Functions.java
+++ Functions.java
@@ -7,7 +7,7 @@ public class Functions {
     }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next()
+        String name = sc.next();
         sc.close();
 
         printMyName(name);
