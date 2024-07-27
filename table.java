e7e9d0788d624d568e0c08c000064031e13d4650
user.email like: "abir030204@gmail.com <abir030204@gmail.com>
2024-07-27 10:43:57 +0530

    added sc.close()

--- table.java
+++ table.java
@@ -4,6 +4,7 @@ public class table {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.close();
 
         for(int i = 1; i < 10; i++){
             System.out.println(n*i);
